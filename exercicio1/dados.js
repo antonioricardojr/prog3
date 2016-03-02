0edad54acfb3214763045f23d10e9cdc06b744ee
Antonio Marques Jr <antonioricardojr@gmail.com>
2016-03-02 19:16:36 -0300

    primeiro exercicio

---
+++ exercicio1/dados.js
@@ -0,0 +1,73 @@
+var db = [
+	{
+		titulo: 'Título do Livro 1',
+		autores: ['Autor 1', 'Autor 2'],
+		descricao: 'Descrição sobre o livro.',
+		url: 'http://urldolivro',
+		preco: 50
+	},
+		{
+		titulo: 'Título do Livro 2',
+		autores: ['Autor 3', 'Autor 4'],
+		descricao: 'Descrição sobre o livro 2.',
+		url: 'http://urldolivro2',
+		preco: 50
+	},
+		{
+		titulo: 'Título do Livro 3',
+		autores: ['Autor 5', 'Autor 6'],
+		descricao: 'Descrição sobre o livro 3.',
+		url: 'http://urldolivro3',
+		preco: 50
+	},
+		{
+		titulo: 'Título do Livro 4',
+		autores: ['Autor 1', 'Autor 8'],
+		descricao: 'Descrição sobre o livro 4.',
+		url: 'http://urldolivro4',
+		preco: 50
+	},
+		{
+		titulo: 'Título do Livro 5',
+		autores: ['Autor 1', 'Autor 2'],
+		descricao: 'Descrição sobre o livro 5.',
+		url: 'http://urldolivro5',
+		preco: 50
+	},
+		{
+		titulo: 'Título do Livro 6',
+		autores: ['Autor 1', 'Autor 2'],
+		descricao: 'Descrição sobre o livro 6.',
+		url: 'http://urldolivro6',
+		preco: 50
+	},
+
+		{
+		titulo: 'Título do Livro 7',
+		autores: ['Autor 1', 'Autor 2'],
+		descricao: 'Descrição sobre o livro 7.',
+		url: 'http://urldolivro7',
+		preco: 11.90
+	},
+		{
+		titulo: 'Título do Livro 8',
+		autores: ['Autor 1', 'Autor 2'],
+		descricao: 'Descrição sobre o livro 8.',
+		url: 'http://urldolivro8',
+		preco: 52.90
+	},
+		{
+		titulo: 'Título do Livro 9',
+		autores: ['Autor 1', 'Autor 2'],
+		descricao: 'Descrição sobre o livro 9.',
+		url: 'http://urldolivro9',
+		preco: 54.90
+	},
+		{
+		titulo: 'Título do Livro 10',
+		autores: ['Autor 1', 'Autor 2'],
+		descricao: 'Descrição sobre o livro 10.',
+		url: 'http://urldolivro10',
+		preco: 99.90
+	}
+]
---
+++ exercicio1/script.js
@@ -0,0 +1,40 @@
+(function(){
+	var app = angular.module('App',[]);
+
+
+	app.controller('livrosController', function ($scope){
+		
+		$scope.livros = db;
+		$scope.indice = 0;
+		$scope.livro = db[$scope.indice];
+
+		$scope.proximo = function () {
+			if ( $scope.indice < db.length - 1){
+				$scope.indice+=1;
+			}else{
+				$scope.indice = 0;
+			}
+			$scope.livro = db[$scope.indice];
+		}
+
+		$scope.anterior = function () {
+			if ( $scope.indice > 0){
+				$scope.indice-=1;
+			}else{
+				$scope.indice = db.length - 1;
+			}
+			$scope.livro = db[$scope.indice];
+		}
+
+		$scope.editar = function () {
+
+
+		}
+
+		$scope.adicionar = function () {
+
+
+		}
+	});
+
+})();
