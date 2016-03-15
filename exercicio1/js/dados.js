c9b047ce5596caff0614d049a004afe945de1ca2
Antonio Marques Jr <antonioricardojr@gmail.com>
2016-03-15 20:46:28 -0300

    adicionando bootstrap

--- exercicio1/js/dados.js
+++ exercicio1/js/dados.js
@@ -4,42 +4,40 @@ var db = [
 		autores: ['Autor 1', 'Autor 2'],
 		descricao: 'Descrição sobre o livro.',
 		url: 'http://urldolivro',
-		preco: 50
+		preco: 50,
+		id: 1
 	},
 		{
 		titulo: 'Título do Livro 2',
 		autores: ['Autor 3', 'Autor 4'],
 		descricao: 'Descrição sobre o livro 2.',
 		url: 'http://urldolivro2',
-		preco: 50
+		preco: 50,
+		id: 2
 	},
 		{
 		titulo: 'Título do Livro 3',
 		autores: ['Autor 5', 'Autor 6'],
 		descricao: 'Descrição sobre o livro 3.',
 		url: 'http://urldolivro3',
-		preco: 50
+		preco: 50,
+		id: 3
 	},
 		{
 		titulo: 'Título do Livro 4',
 		autores: ['Autor 1', 'Autor 8'],
 		descricao: 'Descrição sobre o livro 4.',
 		url: 'http://urldolivro4',
-		preco: 50
-	},
-		{
-		titulo: 'Título do Livro 5',
-		autores: ['Autor 1', 'Autor 2'],
-		descricao: 'Descrição sobre o livro 5.',
-		url: 'http://urldolivro5',
-		preco: 50
+		preco: 50,
+		id: 4
 	},
 		{
 		titulo: 'Título do Livro 6',
 		autores: ['Autor 1', 'Autor 2'],
 		descricao: 'Descrição sobre o livro 6.',
 		url: 'http://urldolivro6',
-		preco: 50
+		preco: 50,
+		id: 5
 	},
 
 		{
@@ -47,27 +45,40 @@ var db = [
 		autores: ['Autor 1', 'Autor 2'],
 		descricao: 'Descrição sobre o livro 7.',
 		url: 'http://urldolivro7',
-		preco: 11.90
+		preco: 11.90,
+		id: 6
 	},
 		{
 		titulo: 'Título do Livro 8',
 		autores: ['Autor 1', 'Autor 2'],
 		descricao: 'Descrição sobre o livro 8.',
 		url: 'http://urldolivro8',
-		preco: 52.90
+		preco: 52.90,
+		id: 7
 	},
 		{
 		titulo: 'Título do Livro 9',
 		autores: ['Autor 1', 'Autor 2'],
 		descricao: 'Descrição sobre o livro 9.',
 		url: 'http://urldolivro9',
-		preco: 54.90
+		preco: 54.90,
+		id: 8
 	},
 		{
 		titulo: 'Título do Livro 10',
 		autores: ['Autor 1', 'Autor 2'],
 		descricao: 'Descrição sobre o livro 10.',
 		url: 'http://urldolivro10',
-		preco: 99.90
-	}
+		preco: 99.90,
+		id: 9
+	},
+
+		{
+			titulo: 'Título do Livro 5',
+			autores: ['Autor 1', 'Autor 2'],
+			descricao: 'Descrição sobre o livro 5.',
+			url: 'http://urldolivro5',
+			preco: 50,
+			id: 10
+		}
 ]
--- exercicio1/js/script.js
+++ exercicio1/js/script.js
@@ -1,16 +1,12 @@
 (function(){
-	var app = angular.module('App',['ui.bootstrap']);
+	var app = angular.module('App',[]);
 
 
 	app.controller('livrosController', function ($scope){
-
-		$scope.itensEditados = {};
 		
 		$scope.livros = db;
 		$scope.indice = 0;
 		$scope.livro = db[$scope.indice];
-
-		$scope.myInterval = 3000;
 
 		$scope.proximo = function () {
 			if ( $scope.indice < db.length - 1){
@@ -30,16 +26,6 @@
 			$scope.livro = db[$scope.indice];
 		}
 
-		$scope.toggleEditing = function(livro){
-    		$scope.itensEditados[livro.id] = 
-    		!$scope.itensEditados[livro.id] || true;
-  		};
-
-		$scope.editar = function (livro){
-			$scope.livro = livro;
-			
-		}
-
 		$scope.update = function () {
 
 			// $scope.livros[$scope.indice] = $scope.livro;
@@ -50,5 +36,13 @@
 
 		}
 	});
+
+
+
+
+
+
+
+
 
 })();
