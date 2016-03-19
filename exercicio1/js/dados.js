2edca43306cdb61480fde6c1110e6e05ed93ade7
Antonio Marques Jr <antonioricardojr@gmail.com>
2016-03-19 08:33:45 -0300

    crud working

--- exercicio1/js/dados.js
+++ exercicio1/js/dados.js
@@ -1,74 +1,74 @@
 var db = [
 	{
-		titulo: 'Título do Livro 1',
-		autores: ['Autor 1', 'Autor 2'],
-		descricao: 'Descrição sobre o livro.',
-		url: 'http://urldolivro',
-		preco: 50
+		titulo: "Harry Potter and the Sorcerer's Stone",
+		autores: ['J.K. Rowling'],
+		descricao: "Harry Potter has no idea how famous he is. That's because he's being raised by his miserable aunt and uncle who are terrified Harry will learn that he's really a wizard, just as his parents were. But everything changes when Harry is summoned to attend an infamous school for wizards, and he begins to discover some clues about his illustrious birthright. From the surprising way he is greeted by a lovable giant, to the unique curriculum and colorful faculty at his unusual school, Harry finds himself drawn deep inside a mystical world he never knew existed and closer to his own noble destiny.",
+		url: 'http://ecx.images-amazon.com/images/I/51PxQCRCx0L._SX412_BO1,204,203,200_.jpg',
+		preco: 14.99
 	},
 		{
-		titulo: 'Título do Livro 2',
-		autores: ['Autor 3', 'Autor 4'],
-		descricao: 'Descrição sobre o livro 2.',
-		url: 'http://urldolivro2',
-		preco: 50
+		titulo: 'A Game of Thrones (A Song of Ice and Fire, Book 1)',
+		autores: ['George R. R. Martin '],
+		descricao: 'From a master of contemporary fantasy comes the first novel of a landmark series unlike any you’ve ever read before. With A Game of Thrones, George R. R. Martin has launched a genuine masterpiece, bringing together the best the genre has to offer. Mystery, intrigue, romance, and adventure fill the pages of this magnificent saga, the first volume in an epic series sure to delight fantasy fans everywhere.',
+		url: 'http://ecx.images-amazon.com/images/I/81w4ELeZqKL.jpg',
+		preco: 18.99
 	},
 		{
-		titulo: 'Título do Livro 3',
-		autores: ['Autor 5', 'Autor 6'],
-		descricao: 'Descrição sobre o livro 3.',
-		url: 'http://urldolivro3',
-		preco: 50
+		titulo: 'Introduction to Algorithms, 3rd Edition',
+		autores: ['Thomas H. Cormen'],
+		descricao: 'Some books on algorithms are rigorous but incomplete; others cover masses of material but lack rigor. Introduction to Algorithms uniquely combines rigor and comprehensiveness. The book covers a broad range of algorithms in depth, yet makes their design and analysis accessible to all levels of readers. Each chapter is relatively self-contained and can be used as a unit of study. The algorithms are described in English and in a pseudocode designed to be readable by anyone who has done a little programming. The explanations have been kept elementary without sacrificing depth of coverage or mathematical rigor.',
+		url: 'http://ecx.images-amazon.com/images/I/51eDwv7tCtL._SX442_BO1,204,203,200_.jpg',
+		preco: 64.99
 	},
 		{
-		titulo: 'Título do Livro 4',
-		autores: ['Autor 1', 'Autor 8'],
-		descricao: 'Descrição sobre o livro 4.',
-		url: 'http://urldolivro4',
-		preco: 50
+		titulo: "The Signal and the Noise: Why So Many Predictions Fail--but Some Don't" ,
+		autores: ['Nate Silver'],
+		descricao: "Nate Silver built an innovative system for predicting baseball performance, predicted the 2008 election within a hair’s breadth, and became a national sensation as a blogger—all by the time he was thirty. He solidified his standing as the nation's foremost political forecaster with his near perfect prediction of the 2012 election. Silver is the founder and editor in chief of FiveThirtyEight.com.",
+		url: 'http://ecx.images-amazon.com/images/I/51Ui-zv3m7L._SX324_BO1,204,203,200_.jpg',
+		preco: 11.95
 	},
 		{
-		titulo: 'Título do Livro 5',
-		autores: ['Autor 1', 'Autor 2'],
-		descricao: 'Descrição sobre o livro 6.',
-		url: 'http://urldolivro6',
-		preco: 50
-	},
-
-		{
-		titulo: 'Título do Livro 6',
-		autores: ['Autor 1', 'Autor 2'],
-		descricao: 'Descrição sobre o livro 7.',
-		url: 'http://urldolivro7',
-		preco: 11.90
-	},
-		{
-		titulo: 'Título do Livro 7',
-		autores: ['Autor 1', 'Autor 2'],
-		descricao: 'Descrição sobre o livro 8.',
-		url: 'http://urldolivro8',
-		preco: 52.90
-	},
-		{
-		titulo: 'Título do Livro 8',
-		autores: ['Autor 1', 'Autor 2'],
-		descricao: 'Descrição sobre o livro 9.',
-		url: 'http://urldolivro9',
-		preco: 54.90
-	},
-		{
-		titulo: 'Título do Livro 9',
-		autores: ['Autor 1', 'Autor 2'],
-		descricao: 'Descrição sobre o livro 10.',
-		url: 'http://urldolivro10',
-		preco: 99.90
+		titulo: 'Applied Predictive Modeling',
+		autores: ['Max Kuhn'],
+		descricao: 'Applied Predictive Modeling covers the overall predictive modeling process, beginning with the crucial steps of data preprocessing, data splitting and foundations of model tuning.  The text then provides intuitive explanations of numerous common and modern regression and classification techniques, always with an emphasis on illustrating and solving real data problems.  Addressing practical concerns extends beyond model fitting to topics such as handling class imbalance, selecting predictors, and pinpointing causes of poor model performance―all of which are problems that occur frequently in practice.',
+		url: 'http://ecx.images-amazon.com/images/I/41bSCvc4VgL._SX329_BO1,204,203,200_.jpg',
+		preco: 81.35
 	},
 
 		{
-			titulo: 'Título do Livro 10',
-			autores: ['Autor 1', 'Autor 2'],
-			descricao: 'Descrição sobre o livro 5.',
-			url: 'http://urldolivro5',
-			preco: 50
+		titulo: 'Analyzing Baseball Data with R',
+		autores: ['Max Marchi'],
+		descricao: 'With its flexible capabilities and open-source platform, R has become a major tool for analyzing detailed, high-quality baseball data. Analyzing Baseball Data with R provides an introduction to R for sabermetricians, baseball enthusiasts, and students interested in exploring the rich sources of baseball data. It equips readers with the necessary skills and software tools to perform all of the analysis steps, from gathering the datasets and entering them in a convenient format to visualizing the data via graphs to performing a statistical analysis.',
+		url: 'http://ecx.images-amazon.com/images/I/41CoBktSCKL._SX329_BO1,204,203,200_.jpg',
+		preco: 37.12
+	},
+		{
+		titulo: 'Big Data Baseball: Math, Miracles, and the End of a 20-Year Losing Streak',
+		autores: ["Travis Sawchik"],
+		descricao: "After twenty consecutive losing seasons for the Pittsburgh Pirates, team morale was low, the club's payroll ranked near the bottom of the sport, game attendance was down, and the city was becoming increasingly disenchanted with its team. Pittsburghers joked their town was the city of champions…and the Pirates. Big Data Baseball is the story of how the 2013 Pirates, mired in the longest losing streak in North American pro sports history, adopted drastic big-data strategies to end the drought, make the playoffs, and turn around the franchise's fortunes.",
+		url: 'http://ecx.images-amazon.com/images/I/51118XdLgVL._SX327_BO1,204,203,200_.jpg',
+		preco: 17.94
+	},
+		{
+		titulo: 'Javascript: The Good Parts',
+		autores: ['Douglas Crockford '],
+		descricao: "Most programming languages contain good and bad parts, but JavaScript has more than its share of the bad, having been developed and released in a hurry before it could be refined. This authoritative book scrapes away these bad features to reveal a subset of JavaScript that's more reliable, readable, and maintainable than the language as a whole—a subset you can use to create truly extensible and efficient code.",
+		url: 'http://ecx.images-amazon.com/images/I/518QVtPWA7L._SX379_BO1,204,203,200_.jpg',
+		preco: 20.81
+	},
+		{
+		titulo: '1001 Guitars to Dream of Playing Before You Die (1001 (Universe))',
+		autores: ['Autor 1', 'Autor 2'],
+		descricao: 'Lavishly illustrated, comprehensive, authoritative—this definitive guide to the most important and groundbreaking guitars is the perfect book for aspiring musicians and serious fans alike. This latest volume in the hugely popular 1001 series surveys the world’s most popular musical instrument. Popular culture has enjoyed an intense love affair with the guitar in both its acoustic and electric forms. Although quite ancient, the guitar only achieved recognition as a serious instrument in the late nineteenth century. Since then, it has become the world’s most consequential instrument, and contemporary music is unimaginable without it. Never before have so many guitars been profiled within a single illustrated volume, from the earliest Spanish baroque models to the latest Gibson Robot series with computer-controlled self-tuning capabilities and onboard sound-processing features. From the classic and obscure to the simple and high-tech, the most notable guitars from Fender, Ibanez, Epiphone, Les Paul, Martin, and numerous other brands are featured in this one-of-a-kind book. Illustrated with striking color photographs, the book reveals what makes each model special, which well-known musicians use it, and the hit songs in which it can be heard. Details about technical and design specifications are included, and lively texts highlight some of the greatest guitar performances of all time.',
+		url: 'http://ecx.images-amazon.com/images/I/51o0hA%2BnmSL._SX379_BO1,204,203,200_.jpg',
+		preco: 27.94
+	},
+
+	{
+			titulo: 'Cracking the Coding Interview, 6th Edition: 189 Programming Questions and Solutions',
+			autores: ['Gayle Laakmann McDowell'],
+			descricao: "I am not a recruiter. I am a software engineer. And as such, I know what it's like to be asked to whip up brilliant algorithms on the spot and then write flawless code on a whiteboard. I've been through this as a candidate and as an interviewer.",
+			url: 'http://ecx.images-amazon.com/images/I/51F6Lwyq5JL._SX348_BO1,204,203,200_.jpg',
+			preco: 33.38
 		}
 ]
--- exercicio1/js/script.js
+++ exercicio1/js/script.js
@@ -26,15 +26,39 @@
 			$scope.livro = db[$scope.indice];
 		}
 
-		$scope.update = function () {
+		$scope.novoLivro = function () {
 
-			// $scope.livros[$scope.indice] = $scope.livro;
+			var novo_livro = {
+				titulo: 'Digite o Título do Livro',
+				autores: ['Digite o autor do livro'],
+				descricao: 'Digite uma descrição sobre o livro',
+				url: '<url da imagem do livro>',
+				preco: -1
+			}
+
+			$scope.livro = novo_livro;
+
 		}
 
-		$scope.adicionar = function () {
+		$scope.cancela = function () {
 
-
+			$scope.livro = db[$scope.indice];
 		}
+
+
+		$scope.adiciona = function () {
+
+			$scope.livros.push($scope.livro);
+			$scope.indice = $scope.livros.length - 1;
+			$scope.livro = $scope.livros[$scope.indice];
+		}
+
+		$scope.remove = function () {
+
+			var livro_removido = $scope.livros.splice($scope.index, 1);
+			$scope.proximo();
+		}
+
 	});
 
 
